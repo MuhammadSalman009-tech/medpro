1fa1f037a9de7004539879202141f8a4b27f717a
MuhammadSalman009-tech <salmansumra009@gmail.com>
2022-04-08 12:36:39 -0700

    complete setup

--- sendMail.php
+++ sendMail.php
@@ -1,19 +1,51 @@
 <?php
 
-/**
- * This example shows settings to use when sending via Google's Gmail servers.
- * This uses traditional id & password authentication - look at the gmail_xoauth.phps
- * example to see how to use XOAUTH2.
- * The IMAP section shows how to save this message to the 'Sent Mail' folder using IMAP commands.
- */
-
-//Import PHPMailer classes into the global namespace
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 
+
+
 require 'vendor/autoload.php';
 
-//Create a new PHPMailer instance
+
+$template = file_get_contents('content.html');
+
+if (isset($_POST['email'])) {
+    $template = str_replace(
+        array("<!-- #{FromState} -->", "<!-- #{FromEmail} -->"),
+        array("Email:", $_POST['email']),
+        $template
+    );
+}
+
+if (isset($_POST['message'])) {
+    $template = str_replace(
+        array("<!-- #{MessageState} -->", "<!-- #{MessageDescription} -->"),
+        array("Message:", $_POST['message']),
+        $template
+    );
+}
+
+// In a regular expression, the character \v is used as "anything", since this character is rare
+preg_match("/(<!-- #\{BeginInfo\} -->)([^\v]*?)(<!-- #\{EndInfo\} -->)/", $template, $matches, PREG_OFFSET_CAPTURE);
+foreach ($_POST as $key => $value) {
+    if ($key != "counter" && $key != "email" && $key != "message" && $key != "form-type" && $key != "g-recaptcha-response" && !empty($value)) {
+        $info = str_replace(
+            array("<!-- #{BeginInfo} -->", "<!-- #{InfoState} -->", "<!-- #{InfoDescription} -->"),
+            array("", ucfirst($key) . ':', $value),
+            $matches[0][0]
+        );
+
+        $template = str_replace("<!-- #{EndInfo} -->", $info, $template);
+    }
+}
+$subject = "A message from your site visitor";
+$template = str_replace(
+    array("<!-- #{Subject} -->", "<!-- #{SiteName} -->"),
+    array($subject, $_SERVER['SERVER_NAME']),
+    $template
+);
+
 $mail = new PHPMailer();
 
 //Tell PHPMailer to use SMTP
@@ -23,7 +55,7 @@ $mail->isSMTP();
 //SMTP::DEBUG_OFF = off (for production use)
 //SMTP::DEBUG_CLIENT = client messages
 //SMTP::DEBUG_SERVER = client and server messages
-$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+$mail->SMTPDebug = SMTP::DEBUG_OFF;
 
 //Set the hostname of the mail server
 $mail->Host = 'smtp.gmail.com';
@@ -63,46 +95,20 @@ $mail->addReplyTo('salmansumra009@gmail.com', 'Muhmmad Salman');
 //Set who the message is to be sent to
 $mail->addAddress('salmansumra009@gmail.com', 'John Doe');
 
-//Set the subject line
-$mail->Subject = 'PHPMailer GMail SMTP test';
 
-//Read an HTML message body from an external file, convert referenced images to embedded,
-//convert HTML into a basic plain-text alternative body
-$mail->msgHTML("Test Html");
 
-//Replace the plain text body with one created manually
-$mail->AltBody = 'This is a plain-text message body';
+// if (isset($_POST['name'])) {
+//     $mail->FromName = $_POST['name'];
+// } else {
+//     $mail->FromName = "Site Visitor";
+// }
 
-//Attach an image file
-// $mail->addAttachment('images/phpmailer_mini.png');
 
-//send the message, check for errors
+$mail->Subject = $subject;
+
+$mail->msgHTML($template);
 if (!$mail->send()) {
     echo 'Mailer Error: ' . $mail->ErrorInfo;
 } else {
-    header("Location:index.php?message=Successfully sent!");
-    //Section 2: IMAP
-    //Uncomment these to save your message in the 'Sent Mail' folder.
-    #if (save_mail($mail)) {
-    #    echo "Message saved!";
-    #}
+    echo ("<script>location.href = 'index.php?message=Successfully sent!';</script>");
 }
-
-//Section 2: IMAP
-//IMAP commands requires the PHP IMAP Extension, found at: https://php.net/manual/en/imap.setup.php
-//Function to call which uses the PHP imap_*() functions to save messages: https://php.net/manual/en/book.imap.php
-//You can use imap_getmailboxes($imapStream, '/imap/ssl', '*' ) to get a list of available folders or labels, this can
-//be useful if you are trying to get this working on a non-Gmail IMAP server.
-// function save_mail($mail)
-// {
-//     //You can change 'Sent Mail' to any other folder or tag
-//     $path = '{imap.gmail.com:993/imap/ssl}[Gmail]/Sent Mail';
-
-//     //Tell your server to open an IMAP connection using the same username and password as you used for SMTP
-//     $imapStream = imap_open($path, $mail->Username, $mail->Password);
-
-//     $result = imap_append($imapStream, $path, $mail->getSentMIMEMessage());
-//     imap_close($imapStream);
-
-//     return $result;
-// }
